74c55cdacc6328f2939b585222cc97d8f6db8b88
DonEduardo27 <michael.schwarzkopf@uni-weimar.de>
2016-08-07 15:38:12 +0200

    Guter Code ist, wenn etwas funktioniert, bevor man damit rechnet.

--- framework/surface.cpp
+++ framework/surface.cpp
@@ -7,7 +7,7 @@ Surface::Surface(glm::vec3 const& _positionVector, glm::vec3 const& _orientation
 	norm{_norm},
 	length{_length},
 	width{_width}{}
-	
+
 Surface::Surface(glm::vec3 const& _positionVector, glm::vec3 const& _orientation, glm::vec3 const& _norm, float _length, float _width, std::string const& _name, Material const& _mat):
 	positionVector{_positionVector},
 	orientation{_orientation},
@@ -37,7 +37,8 @@ Hit Surface::intersect(Ray const& ray)
 		float distance = (-(norm.x*(ray.origin.x - positionVector.x))-(norm.y*(ray.origin.y - positionVector.y))
 			-(norm.z*(ray.origin.z - positionVector.z))) / denominator;
 		if(distance > 0)
-		{
+		{ 
+			
 			hit.impact = true;
 			hit.point  = ray.origin + (distance * ray.direction);
 			hit.normal = norm;
