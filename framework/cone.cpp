889cea6114f1ec17c9992501d157d895988999ae
Fabi3nn3 <fabienne.hubricht@uni-weimar.de>
2016-08-15 20:15:19 +0200

    Added cone hpp and cpp, missing intersect

---
+++ framework/cone.cpp
@@ -0,0 +1,82 @@
+#include "cone.hpp"
+#include "vektoroperations.hpp"
+#include <math.h>
+#include <string>
+
+Cone::Cone():
+	Shape{"Cone",{}},ctr_{0.0f,0.0f,0.0f}, rad_{0.0f}, peak_{0.0f,0.0f,0.0f}{
+
+	}
+
+Cone::Cone(glm::vec3 const& ctr, float rad, glm::vec3 const& peak):
+	Shape{"Cone",{}},ctr_{ctr}, rad_{rad}, peak_{peak}{
+
+	}
+
+Cone::Cone(glm::vec3 const& ctr, float rad, glm::vec3 const& peak, std::string const& name, Material const& mat):
+	Shape{name, mat},ctr_{ctr}, rad_{rad}, peak_{peak}{
+
+	}
+
+Cone::~Cone(){
+
+	std::cout << "Delete Cone" <<std::endl;
+}
+
+std::ostream & Cone::print(std :: ostream & os) const{
+
+	Cone::print(os);
+	os<<"Location: ("
+	<<ctr_.x << " , "
+	<<ctr_.y << " , "
+	<<ctr_.z << " , Radius: ("
+	<<rad_ << ") Hoehe: ( "
+	<<peak_.x<< " , "
+	<<peak_.y<< " , "
+	<<peak_.z<< " ) "<<std::endl;
+}
+
+glm::vec3 const& Cone::getctr() const{
+
+	return ctr_;
+}
+
+float const& Cone::getrad() const{
+
+	return rad_;
+}
+
+glm::vec3 const& Cone::getpeak() const{
+
+	return peak_;
+}
+
+void Cone::setctr(glm::vec3 const& ctr){
+
+	ctr_ = ctr;
+}
+
+void Cone::setrad(float rad){
+
+	rad_ = rad;
+}
+
+void Cone::setpeak(glm::vec3 const& peak){
+
+	peak_ = peak;
+}
+
+/*Hit Cone::intersect(Ray const& ray){
+
+	Hit coneHit;
+
+	glm::vec3 l = ray.direction;
+	glm::vec3 o = ray.origin;
+	glm::vec3 c = ctr_;
+	float 	  r = rad_;
+	glm::vec3 p = peak_; 
+
+	
+
+
+}*/
---
+++ framework/cone.hpp
@@ -0,0 +1,36 @@
+#ifndef CONE_HPP
+#define CONE_HPP
+
+#include "shape.hpp"
+#include "surface.hpp"
+#include <vector>
+#include <memory>
+#include <glm/vec3.hpp>
+
+class Cone: public Shape
+{
+public:
+	Cone();
+	Cone(glm::vec3 const& ctr, float rad, glm::vec3 const& peak);
+	Cone(glm::vec3 const& ctr, float rad, glm::vec3 const& peak, std::string const& name, Material const& mat);
+	~Cone();
+
+	//Hit intersect (Ray const& ray) override;
+
+	std::ostream & print ( std :: ostream & os) const override;
+	glm::vec3 const& getctr()    const;
+	float     const& getrad()    const;
+	glm::vec3 const& getpeak() const;
+	void setctr(glm::vec3 const& ctr);
+	void setrad(float rad);
+	void setpeak(glm::vec3 const& peak);
+
+
+private:
+	glm::vec3 ctr_;
+	float     rad_;
+	glm::vec3 peak_;
+
+};
+
+#endif 
