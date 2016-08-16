1cd153f031b31027e90700c1eb10103c7b36eba2
Arduqq <twop@hotmail.de>
2016-08-16 13:04:31 +0200

    Added: Composite + Checkliste zur Einbeziehung in vorhandenen Code

---
+++ framework/composite.cpp
@@ -0,0 +1,26 @@
+#include "composite.hpp"
+
+Composite::Composite(){}
+Composite::~Composite(){}
+
+/*
+-> Fügt ein Shape (Box, Sphere, Composite) am Anfang der Map an
+*/
+void Composite::addChild(std::shared_ptr<Shape>& shape){
+	shapes_.insert(shapes_.begin(),std::pair<std::string, std::shared_ptr<Shape>>(shape->getname(),shape));
+}
+
+void Composite::removeChild(std::string const& name){
+	shapes_.erase(name);
+}
+
+std::ostream& Composite::print(std::ostream os) const{
+	for(auto x: shapes_) {
+		x.second->print(os); //x.second = zweite Spalte von x
+	}
+	return os;
+}
+
+std::map<std::string, std::shared_ptr<Shape>> Composite::getChild(){
+	return shapes_;
+}
---
+++ framework/composite.hpp
@@ -0,0 +1,51 @@
+#ifndef COMPOSITE_HPP
+#define COMPOSITE_HPP
+
+#include <map>
+#include <string>
+#include <iostream>
+#include <memory>
+#include "Shape.hpp"
+#include "Box.hpp"
+#include "Sphere.hpp"
+
+/*
+Composite (7.2)
+-------------------------------------------------------------
+-> Composite-Pattern entspricht Baumstruktur
+
+          --Composite-Box
+Composite-|			  -- Sphere
+		  --Composite-|
+		  			  -- Box
+
+-> Zusammensetzungen von Objekten können
+   können uniform behandelt werden
+
+http://www.bogotobogo.com/DesignPatterns/composite.php
+-> Knoten müssen aus Composites bestehen, Blätter sind Shapes
+-------------------------------------------------------------
+[] Sceneloader muss um ein weiteres if erweitert werden
+[] define shape composite [Knoten] [Blatt_1] ... [Blatt_n]
+[] Endboss: Composites müssen gerendert werden
+[] Könnte vielleicht ein paar Tests vertragen (*)
+[] Composites aus Composites aus Composites möglich? (opt.)
+*/
+
+class Composite : public Shape{
+public:
+	Composite();
+	~Composite();
+
+	void addChild(std::shared_ptr<Shape>& shape); //*
+	void removeChild(std::string const& name); //*
+
+	std::ostream& print(std::ostream os) const;
+
+	std::map<std::string, std::shared_ptr<Shape>> getChild(); //*
+
+private:
+	std::map<std::string,std::shared_ptr<Shape>> shapes_;
+};
+
+#endif
